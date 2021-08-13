26f542eebcb5c28c60cf9358ef47e9277ab380ed
I'm Groot <80740629+nlmupdy01@users.noreply.github.com>
2021-08-13 09:49:54 +0530

    Add files via upload

---
+++ Python  CHALLENGE/Alternating Characters.py
@@ -0,0 +1,24 @@
+import os
+
+# Complete the alternatingCharacters function below.
+def alternatingCharacters(s):
+    deletions = 0
+
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            deletions += 1
+
+    return deletions
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
