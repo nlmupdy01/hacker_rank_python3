0250fe3c0d02607917251b0893ba5f589a9058ab
I'm Groot <80740629+nlmupdy01@users.noreply.github.com>
2021-09-08 09:14:29 +0530

    Add files via upload

---
+++ Python  CHALLENGE/Greedy Florist.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(k, c):
+    c.sort(reverse=True)
+    cost = 0
+    previous_purchase = 0
+    for i in range(n):
+        cost += (previous_purchase +1) * c[i]
+        if (i+1)%k==0:
+            previous_purchase += 1
+    return cost
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
